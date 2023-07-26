db23937ca98e40f2b8ae3772d8be03f63485fa2d
Aditya Kadam <79622885+adii-tech@users.noreply.github.com>
2023-07-26 13:48:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function volume_sphere() {
     //Write your code here
-  
+   let radius=document.getElementById('radius').value;
+  console.log(radius);
+  let ans;
+  if(radius){
+  // ans=(4*3.14*radius*radius*radius)/3;
+  ans=(4*Math.PI*Math.pow(radius,3))/3;
+  }
+  let volume=document.getElementById('volume');
+//   console.log(volume);
+if(ans!=undefined){
+    volume.value=ans.toFixed(4);
+}
+return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
